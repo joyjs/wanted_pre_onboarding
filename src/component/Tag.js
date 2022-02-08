0ec49b201280d17a6069d62ca73524afc231be28
Jinnie Cho <yejincho1105@gmail.com>
2022-02-08 16:43:38 +0900

    💬 Updated className of Tag components

--- src/component/Tag.js
+++ src/component/Tag.js
@@ -5,22 +5,25 @@ function Tag() {
     <section className="Tag">
       <h1 className="title">Tag</h1>
 
-      <div className="tags-stack">
-        <ul>
-          <li>
-            <p>CodeStates</p>
-            <p>x</p>
-          </li>
-          <li>
-            <p>JJang</p>
-            <p>x</p>
-          </li>
-        </ul>
-        <input
-          type="text"
-          placeholder="Please enter to add tags"
-          // onKeyUp={addTag}
-        />
+      <div className="tags-wrapper">
+        <div className="tags-stack">
+          <ul className="tags">
+            <li className="tag">
+              <p className="tag-text">CodeStates</p>
+              <p className="tag-close-button">x</p>
+            </li>
+            <li className="tag">
+              <p className="tag-text">JJang</p>
+              <p className="tag-close-button">x</p>
+            </li>
+          </ul>
+          <input
+            className="tags-stack-input"
+            type="text"
+            placeholder="Please enter to add tags"
+            // onKeyUp={addTag}
+          />
+        </div>
       </div>
     </section>
   )
