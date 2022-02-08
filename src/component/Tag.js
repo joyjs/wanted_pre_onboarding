87bb4b43d5d0e2e52b9cf57fb3ccea404f1e4c46
Jinnie Cho <yejincho1105@gmail.com>
2022-02-08 18:23:31 +0900

    🔨 Implemented scripts to Tag component

--- src/component/Tag.js
+++ src/component/Tag.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Tag() {
+function Tag(props) {
+  const [tags, setTags] = useState(['CodeStates', 'JJang'])
+
+  const removeTag = (tagToRemove) => {
+    setTags(tags.filter((_, i) => i !== tagToRemove))
+  }
+
+  const addTag = (e) => {
+    if (e.target.value !== '') {
+      setTags([...tags, e.target.value])
+      props.selected([...tags, e.target.value])
+      e.target.value = ''
+    }
+  }
+
   return (
     <section className="Tag">
       <h1 className="title">Tag</h1>
@@ -8,24 +22,24 @@ function Tag() {
       <div className="tags-wrapper">
         <div className="tags-stack">
           <ul className="tags">
-            <li className="tag">
-              <p className="tag-text">CodeStates</p>
-              <button className="tag-close-button" type="button">
-                x
-              </button>
-            </li>
-            <li className="tag">
-              <p className="tag-text">JJang</p>
-              <button className="tag-close-button" type="button">
-                x
-              </button>
-            </li>
+            {tags.map((tags, i) => (
+              <li className="tag" key={i}>
+                <p className="tag-text">{tags}</p>
+                <button
+                  className="tag-close-button"
+                  type="button"
+                  onClick={() => removeTag(i)}
+                >
+                  x
+                </button>
+              </li>
+            ))}
           </ul>
           <input
             className="tags-stack-input"
             type="text"
             placeholder="Please enter to add tags"
-            // onKeyUp={addTag}
+            onKeyUp={(e) => (e.key === 'Enter' ? addTag(e) : null)}
           />
         </div>
       </div>
