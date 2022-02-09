2cfcb67eee0f237bc6a7910bb84a34ede7160bb6
Jinnie Cho <yejincho1105@gmail.com>
2022-02-09 20:36:55 +0900

    🔨 Implemented scripts to ClickToEdit component

--- src/component/ClickToEdit.js
+++ src/component/ClickToEdit.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function ClickToEdit() {
+function ClickToEdit(props) {
+  const handleClick = (e) => {
+    e.target.select()
+  }
+
+  const [name, setName] = useState('김코딩')
+  const [age, setAge] = useState(20)
+
   return (
     <section className="ClickToEdit">
       <h1 className="title">ClickToEdit</h1>
@@ -17,6 +24,10 @@ function ClickToEdit() {
               type="text"
               minLength={2} // NOTE: 이름의 기재문자 수의 제한 예외 사항으로 인해 maxLength 대신 minLength를 적용함(https://bit.ly/생활법령정보_이름관련준수사항)
               required
+              placeholder="이름을 입력하세요"
+              onClick={handleClick}
+              onChange={(e) => setName(e.target.value)}
+              value={name}
             />
           </div>
 
@@ -30,6 +41,10 @@ function ClickToEdit() {
               type="text"
               maxLength={3}
               required
+              placeholder="나이를 입력하세요"
+              onClick={handleClick}
+              onChange={(e) => setAge(e.target.value)}
+              value={age}
             />
           </div>
         </form>
@@ -37,12 +52,12 @@ function ClickToEdit() {
         <div className="clicktoedit-results">
           <div className="result-name">
             <p className="name-title">이름</p>
-            <p className="name-binded">최코딩</p>
+            <p className="name-binded">{name}</p>
           </div>
 
           <div className="result-age">
             <p className="age-title">나이</p>
-            <p className="age-binded">55 </p>
+            <p className="age-binded">{age}</p>
           </div>
         </div>
       </div>
